d2d4933ab8e72bf99923f284fc444687989b6702
JAGADEESH UNDAVALLI <84311163+UndavalliJagadeesh@users.noreply.github.com>
2023-07-10 11:42:37 +0530

    Delete variable.rs

--- Source Codes/variable.rs
+++
@@ -1,9 +0,0 @@
-fn main(){
-    let mut x=10;
-    let y=100;
-    x=x+10;
-    // y=y+10; ==> y is immutable. Can be changed as below
-    let y = y+10; // shadowing
-    println!("The value of x is {x}");
-    println!("The value of y is {y}");
-}
